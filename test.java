9c2d58b55f73d5f6fcbe31f5dd1cd239171bcb20
Cameron Mitchell <cm017@lvc.edu>
2021-02-12 13:41:16 -0500

    Commit on main

---
+++ test.java
@@ -0,0 +1,14 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello from main");
+        int num = 25;
+        System.out.println("The value of the num is: " + num);
+    }
+
+    public static void testMethod() {
+        System.out.println("Testing some code for the main branch");
+        System.out.println("Testing some more code");
+        double val = 3.2434;
+        System.out.println("The value of val is: " + val);
+    }
+}
