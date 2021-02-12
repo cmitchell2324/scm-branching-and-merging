4ede97e44cf917142b120ad11da8459c29cece68
Cameron Mitchell <cm017@lvc.edu>
2021-02-12 13:45:36 -0500

    Commit

--- test.java
+++ test.java
@@ -11,11 +11,4 @@ public class test {
         double val = 3.2434;
         System.out.println("The value of val is: " + val);
     }
-
-    public static void experimentalMethod() {
-        System.out.println("This is some code I did in the test-branch.");
-        System.out.println("I'm trying some stuff out here");
-        boolean isTrue = true;
-        System.out.println(isTrue);
-    }
 }
